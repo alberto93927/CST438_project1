ca11b34a2245c3876a27f9b747d72b17ff9ee167
James Lindfors <jameslindfors@outlook.com>
2025-02-12 19:59:43 -0800

    Created flexzone header component

--- app/auth.tsx
+++ app/auth.tsx
@@ -5,30 +5,36 @@ import { ThemedText } from "@/components/ThemedText";
 import { useSession } from "@/hooks/ctx";
 import { router } from "expo-router";
 
+import FlexZoneHeader from "@/components/FlexZoneHeader";
+
 export default function AuthScreen() {
-    const { signIn } = useSession();
+  const { signIn } = useSession();
 
-    const handleSignIn = async () => {
-        await signIn();
-        router.replace("/");
-    };
+  const handleSignIn = async () => {
+    await signIn();
+    router.replace("/");
+  };
 
-    return (
-        <ThemedView style={styles.container}>
-            <ThemedText type="title">Fitness App</ThemedText>
-            <Button
-                title="Sign In With Google"
-                onPress={handleSignIn}
-            />
-        </ThemedView>
-    );
+  return (
+    <ThemedView style={styles.container}>
+      <FlexZoneHeader />
+      <ThemedView>
+        <Button
+          title="Sign In With Google"
+          onPress={handleSignIn}
+        />
+      </ThemedView>
+      <ThemedView>
+        <ThemedText type="default">Group 8 - CST438</ThemedText>
+      </ThemedView>
+    </ThemedView>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        gap: 64
-    },
+  container: {
+    flex: 1,
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
 });
