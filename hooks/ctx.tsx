06885ac8e1ee0a45deea60fb7a89374443fcb829
James Lindfors <jameslindfors@outlook.com>
2025-02-15 20:59:34 -0800

    Refactor user creation and retrieval logic to use SQLite directly; implement googleSignIn function for session management.

--- hooks/ctx.tsx
+++ hooks/ctx.tsx
@@ -1,6 +1,7 @@
-import { useContext, createContext, type PropsWithChildren, useEffect, useState, useCallback } from 'react';
+import { useContext, createContext, type PropsWithChildren, useEffect } from 'react';
 import { useStorageState } from './useStorageState';
 import { GoogleSignin, isErrorWithCode, isSuccessResponse, statusCodes } from '@react-native-google-signin/google-signin';
+import { createUser, getUserByGoogleId } from '@/db/user';
 
 const AuthContext = createContext<{
   signIn: () => void;
@@ -36,34 +37,25 @@ export function SessionProvider({ children }: PropsWithChildren) {
     <AuthContext.Provider
       value={{
         signIn: async () => {
-          try {
-            await GoogleSignin.hasPlayServices();
-            const response = await GoogleSignin.signIn();
+          // Call the google sign in method
+          const user = await googleSignIn();
+          if (!user) throw new Error('User not found');
 
-            if (isSuccessResponse(response)) {
-              setSession(JSON.stringify(response.data.user));
-            } else {
-              console.error('Sign in was cancelled');
-            }
-          } catch (error) {
-            console.error(error);
-            if (isErrorWithCode(error)) {
-              switch (error.code) {
-                case statusCodes.IN_PROGRESS:
-                  console.error("Sign-in already in progress");
-                  break;
-                case statusCodes.PLAY_SERVICES_NOT_AVAILABLE:
-                  console.error(
-                    "Play services are not available. Please install Play services from the Play Store."
-                  );
-                  break;
-                default:
-                  console.error("An error occurred while signing in");
-                  break;
-              }
-            } else {
-              console.error("An error occurred while signing in");
-            }
+          // Check if the db has the user
+          const dbUser = await getUserByGoogleId(user.id);
+
+          if (dbUser) {
+            // Set the session to be the userID
+            setSession(dbUser.id);
+          } else {
+            // Create the user
+            const newUser = await createUser({
+              g_id: user.id,
+              username: user.name || user.givenName + ' ' + user.familyName,
+              email: user.email,
+              profile_pic: user.photo,
+            });
+            setSession(newUser.toString());
           }
         },
         signOut: async () => {
@@ -81,4 +73,35 @@ export function SessionProvider({ children }: PropsWithChildren) {
       {children}
     </AuthContext.Provider>
   );
+}
+
+const googleSignIn = async () => {
+  try {
+    await GoogleSignin.hasPlayServices();
+    const response = await GoogleSignin.signIn();
+
+    if (isSuccessResponse(response)) {
+      return response.data.user;
+    } else {
+      console.error('Sign in was cancelled');
+    }
+  } catch (error) {
+    if (isErrorWithCode(error)) {
+      switch (error.code) {
+        case statusCodes.IN_PROGRESS:
+          console.error("Sign-in already in progress");
+          break;
+        case statusCodes.PLAY_SERVICES_NOT_AVAILABLE:
+          console.error(
+            "Play services are not available. Please install Play services from the Play Store."
+          );
+          break;
+        default:
+          console.error("An error occurred while signing in");
+          break;
+      }
+    } else {
+      console.error("An error occurred while signing in");
+    }
+  }
 }
